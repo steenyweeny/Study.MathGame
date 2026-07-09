084a28ff7d6bfc7dd0b10a794dc3133bf45ada90
steenyweeny <149864766+steenyweeny@users.noreply.github.com>
2026-07-09 17:38:23 +1200

    updated main question logic to sit in helper class. cleaned up how questions are generated and answered
    added difficulty levels

---
+++ GameHelpers.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Reflection.Emit;
+
+public static class GameHelpers
+{
+    // random symbol generator function to select a symbol for the math equation
+    public static string GenerateSymbol()
+    {
+        Random symbolGen = new Random();
+        int symbolSelected = symbolGen.Next(1, 5);
+
+        switch (symbolSelected)
+        {
+            case 1: return "+";
+            case 2: return "-";
+            case 3: return "*";
+            case 4: return "/";
+            default: return "+";
+        }
+    }
+
+    // function to check the answer based on the symbol and return the correct answer
+    public static int checkAnswer(int num1, int num2, char sym)
+    {
+        switch (sym)
+        {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            default:
+                throw new InvalidOperationException("Invalid symbol");
+        }
+    }
+
+    //public static int DifficultyLevel(int difficulty)
+    //{
+    //    
+    //}
+
+}
